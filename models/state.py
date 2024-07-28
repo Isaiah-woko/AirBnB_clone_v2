aada018f55c7fb81893834b78493009c4cbd9364
Isaiah woko <wokoisaiah68@gmail.com>
2024-07-28 22:55:51 +0100

    task 7

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from models import storage_ob
 from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
@@ -18,3 +20,14 @@ class State(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """Initializes user"""
         super().__init__(*args, **kwargs)
+
+    # getter method
+    @property
+    def cities(self):
+        """return the cities"""
+        if storage_ob != 'db':
+            cities = models.storage.all(City)
+            cities_status = [c for c in cities.values() 
+                             if c.state_id == self.id]
+            return cities_status
+        return []
